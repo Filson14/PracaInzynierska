b0264f862b975aff63749f0768d57f90f9ac6b77
Filson14 <fpasternak14@gmail.com>
2016-01-03 03:04:24 +0100

    Interpolacja
    Interpolowane wartości istotnych punktów (węzły granicy zlewni, punkty
    przecięcia granicy z liniami siatki). Organizacja wyznaczonych punktów w
    matrycy. Triangulacja na bazie nowych punktów - wyznaczenie trójkątów,
    dla których objętość bryły opadu należy obliczyć.

--- matlab/dataTriangulation.m
+++ matlab/dataTriangulation.m
@@ -1,87 +1,58 @@
-%%
-%    clear('rainfallData');
-%     rainfallData = readRainfallData(rainfallStations);
+%% Sprz¹tanie
+clear;
+clc;
+close all;
 
 %% Przygotowanie danych do triangulacji
+disp('Przygotowanie danych do obliczeñ.');
+load ('.\zapisane dane\rainfallStations.mat');
+load ('.\zapisane dane\catchment.mat');
 stationsAmmount = length(rainfallStations);
-triangulation_X = zeros(stationsAmmount, 1);
-triangulation_Y = zeros(stationsAmmount, 1);
-% triangulation_Z = zeros(stationsAmmount, 1);
+
+clear('rainfallPoints');
+clear('borderPoints');
+clear('interpolationPoints');
+clear('poinsInsideCatchment');
+rainfallPoints = zeros(stationsAmmount, 3);
+interpolationPoints = [];
+borderPoints = [];
 for i = 1:stationsAmmount
-    triangulation_X(i, 1) = rainfallStations(1, rainfallData(i).stationIndex).longitude;
-    triangulation_Y(i, 1) = rainfallStations(1, rainfallData(i).stationIndex).latitude;
-    
-    %     triangulation
-    triangulation_Z(i, 1) = rand(1) * 10;
-    %%% Wektor wartoœci dla triangulacji uzupe³niany w³aœciwymi wartoœciami
-    %     if isempty(rainfallData(i).hourly)
-    %         triangulation_Z(i, 1) = 100;
-    %     else
-    %         triangulation_Z(i, 1) = rainfallData(i).hourly.value;
-    %     end;
-    %%%
+    rainfallPoints(i, 1) = rainfallStations(1, i).longitude;
+    rainfallPoints(i, 2) = rainfallStations(1, i).latitude;
+    rainfallPoints(i, 3) = rand(1) * 10;
 end
 
+%% Triangulacja punktów
+disp('Triangulacja punktów wejœciowych.');
 figure
-dt = delaunayTriangulation(triangulation_X,triangulation_Y);
-
+hold on;
+dt = delaunayTriangulation(rainfallPoints(:, 1), rainfallPoints(:, 2));
 triplot(dt, 'y');
 
-%% Labelki elementów na wykresie
-% tetramesh(dt,'FaceColor',[0.6875 0.8750 0.8984],'FaceAlpha',0.3);
-%
-% Display the Vertex and Triangle labels on the plot
-hold on
-% vxlabels = arrayfun(@(n) {sprintf('%d', n)}, (1:stationsAmmount)');
-% Hpl = text(triangulation_X, triangulation_Y, vxlabels, 'FontWeight', 'bold', 'HorizontalAlignment',...
-%     'center', 'BackgroundColor', 'none');
-% ic = incenter(dt);
-% % numtri = size(dt,1);
-% % trilabels = arrayfun(@(x) {sprintf('T%d', x)}, (1:numtri)');
-% % Htl = text(ic(:,1), ic(:,2), trilabels, 'FontWeight', 'bold', ...
-% %    'HorizontalAlignment', 'center', 'Color', 'blue');
-
 %% Rysowanie obszaru zlewni na wykresie
-% catchmentsAmmount = length(catchments);
-% 
-% for i = 1:catchmentsAmmount
-%     zlewnia = catchments{i};
-%     clear('x');
-%     clear('y');
-% 
-%     pointsAmmount = length(zlewnia.p);
-%     %     x = zeros(pointsAmmount, 1);
-%     %     y = zeros(pointsAmmount, 1);
-%     a = 1;
-%     for i = 1:pointsAmmount
-%         x(a) = zlewnia.p{1,i}.x;
-%         y(a) = zlewnia.p{1,i}.y;
-%         a = a + 1;
-%     end
-%     plot(x,y,'-r');
-% end
-%% Zlewnia warty do welny
-clear('x');
-clear('y');
+disp('Zlewnia na wykresie');
+plot(catchment(:,1), catchment(:, 2),'-r');
 
-zlewnia.p = Warta_do_Welny;
-pointsAmmount = length(zlewnia.p);
-%     x = zeros(pointsAmmount, 1);
-%     y = zeros(pointsAmmount, 1);
-a = 1;
-% Na potrzeby analizy zmniejszam dok³adnoœæ granic zlewni.
-clear('catchment');
-for i = 1:5:pointsAmmount
-    catchment(a, 1) = zlewnia.p{1, i}.x;
-    catchment(a, 2) = zlewnia.p{1, i}.y;
-    x(a) = zlewnia.p{1,i}.x;
-    y(a) = zlewnia.p{1,i}.y;
-    a = a + 1;
+%% Wyznaczanie wektorów normalnych dla poszczególnych trójk¹tów
+disp('Wyznaczanie wektorów normalnych dla p³aszczyzn poszczególnych trójk¹tów.');
+numtri = length(dt.ConnectivityList);
+triangleVectors = zeros(numtri, 3);
+for i = 1:numtri
+    triangle = dt.ConnectivityList(i, :);
+    A = [rainfallPoints(triangle(1), 1), rainfallPoints(triangle(1), 2), rainfallPoints(triangle(1), 3)];
+    B = [rainfallPoints(triangle(2), 1), rainfallPoints(triangle(2), 2), rainfallPoints(triangle(2), 3)];
+    C = [rainfallPoints(triangle(3), 1), rainfallPoints(triangle(3), 2), rainfallPoints(triangle(3), 3)];
+
+    AB = [B(1) - A(1), B(2) - A(2), B(3) - A(3)];
+    BC = [C(1) - B(1), C(2) - B(2), C(3) - B(3)];
+
+    triangleVectors(i,:) = cross(AB, BC);
 end
-plot(x,y,'-r');
 
-%% Punkty przeciêcia granicy zlewni z odninkami triangulacji
-% end
+%% Punkty przeciêcia granicy zlewni z odcinkami triangulacji
+disp('Interpolacja wartoœci dla wêz³ów granic zlewni oraz punktów przeciêcia granicy zlewni z liniami triangulacji.');
+x = catchment(:, 1);
+y = catchment(:, 2);
 ilPunktow = length(x);
 
 for i = 1:ilPunktow
@@ -89,17 +60,29 @@ for i = 1:ilPunktow
         line = [x(i-1) y(i-1) x(i) y(i)];
         numtri = length(dt.ConnectivityList);
         for j = 1:numtri
+            triangle = dt.ConnectivityList(j, :);
+            
+            triangle_vx = [rainfallPoints(triangle(1),1),rainfallPoints(triangle(2),1),rainfallPoints(triangle(3),1)];
+            triangle_vy = [rainfallPoints(triangle(1),2),rainfallPoints(triangle(2),2),rainfallPoints(triangle(3),2)];
+            
+            if max(inpolygon(x(i), y(i), triangle_vx, triangle_vy)) == 1
+                point_value = findValueFromSurface(x(i), y(i), triangleVectors(j,:), rainfallPoints(triangle(1),:));
+                borderPoints = [borderPoints; [x(i), y(i), point_value]];
+                h=plot(x(i),y(i),'bo','MarkerSize',5,'LineWidth',1);
+            end
+            
             % TODO: Dla przyspieszenia mo¿na zapisywaæ sprawdzone odcinki
             % trójk¹tów ¿eby nie powtarzaæ.
             for k = 1:3
-                endOne = rainfallStations(dt.ConnectivityList(j, k));
-                endTwo = rainfallStations(dt.ConnectivityList(j, mod(k, 3) + 1));
-                line2 = [endOne.longitude endOne.latitude endTwo.longitude endTwo.latitude];
+                endOne = rainfallPoints(triangle(k), :);
+                endTwo = rainfallPoints(triangle(mod(k, 3) + 1), :);
+                line2 = [endOne(1) endOne(2) endTwo(1) endTwo(2)];
                 [intersect_x, intersect_y] = lineintersect(line, line2);
                 if ~isnan(intersect_x)
                     h=plot(intersect_x,intersect_y,'rx','MarkerSize',10,'LineWidth',2);
+                    intersect_z = findValueFromSurface(intersect_x, intersect_y, triangleVectors(j,:), rainfallPoints(triangle(1),:));
+                    interpolationPoints = [interpolationPoints; [intersect_x, intersect_y, intersect_z]];
                 end
-                
             end
         end
     end
@@ -108,8 +91,32 @@ end;
 ylabel('Szerokoœæ geograficzna')
 xlabel('D³ugoœæ geograficzna')
 
-[lolx, loly] = stationsInsideCatchment(x, y, rainfallStations)
-h=plot(lolx,loly,'ro','MarkerSize',10,'LineWidth',2);
+%% Punkty dane znajduj¹ce siê wewn¹trz obszaru zlewni
+pointsInsideCatchment = stationsInsideCatchment(x, y, rainfallPoints);
+triPoints = [borderPoints; interpolationPoints; pointsInsideCatchment];
+h=plot(pointsInsideCatchment(:,1),pointsInsideCatchment(:,2),'ro','MarkerSize',5,'LineWidth',1);
 
+%% Druga triangulacja - punkty dane i interpolowane.
+%  Wyznaczenie opadu powierzchniowego
+disp('Triangulacja wraz z punktami interpolowanymi.');
+figure
+hold on;
 
-hold off
+boarderPtsAmmount = length(borderPoints)
+
+C = [(1:(boarderPtsAmmount - 1))' (2:boarderPtsAmmount)'; boarderPtsAmmount 1];
+triangulation = delaunayTriangulation(triPoints(:, 1), triPoints(:, 2), C);
+triplot(triangulation, 'b');
+plot([triPoints(1:boarderPtsAmmount,1); triPoints(1,1)], [triPoints(1:boarderPtsAmmount,2); triPoints(1,2)], '-r','LineWidth',2);
+
+%% Wyznaczenie trójk¹tów wewn¹trz granicy
+figure;
+hold on;
+IO = isInterior(triangulation);
+properTriangles = triangulation(IO, :);
+triplot(triangulation(IO, :),triangulation.Points(:,1), triangulation.Points(:,2),'LineWidth', 1)
+plot(triPoints(C'),triPoints(C'+size(triPoints,1)),'-r','LineWidth', 2);
+
+% %% Pole wieloboku - polyarea
+% U¿yæ properTriangles i triPoints
+
--- matlab/stationsInsideCatchment.m
+++ matlab/stationsInsideCatchment.m
@@ -1,19 +1,8 @@
-function [ stationsInsideX, stationsInsideY ] = stationsInsideCatchment( x, y, stations )
+function [ stationsInside ] = stationsInsideCatchment( x, y, stations )
 %STATIONSINSIDECATCHMENT Summary of this function goes here
 %   Detailed explanation goes here
     stationsCount = length(stations);
-
-    xq = zeros(stationsCount,1);
-    yq = zeros(stationsCount,1);
-    
-    for i = 1:stationsCount
-       xq(i) = stations(i).longitude;
-       yq(i) = stations(i).latitude;
-    end
-    
-    [in, on] = inpolygon(xq, yq, x, y);   
-    
-    stationsInsideX = vertcat(xq(in), xq(on));
-    stationsInsideY = vertcat(yq(in), yq(on));
+    in = inpolygon(stations(:,1), stations(:,2), x, y);   
+    stationsInside = stations(in,:)
 end
 
